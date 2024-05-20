8daf157ddbd279a7474f346c4fa197ddfa1c48b2
RCPawn <168956969+RCPawn@users.noreply.github.com>
2024-05-20 12:47:32 +0800

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import com.RCPawn.ui.LoginJFrame;
+
+public class App {
+    public static void main(String[] args) {
+        // 程序的启动入口
+        new LoginJFrame();
+    }
+}
---
+++ src/com/RCPawn/Test/Test4.java
@@ -0,0 +1,7 @@
+package com.RCPawn.Test;
+
+public class Test4 {
+    public static void main(String[] args) {
+        new MyJFrame3();
+    }
+}
